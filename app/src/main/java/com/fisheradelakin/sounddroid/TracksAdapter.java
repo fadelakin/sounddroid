9f9584613720db34ec047f7bf482333628d62dbc
Fisher Adelakin <temidayoadelakin@gmail.com>
2015-06-20 18:18:28 -0400

    cleanup

--- app/src/main/java/com/fisheradelakin/sounddroid/TracksAdapter.java
+++ app/src/main/java/com/fisheradelakin/sounddroid/TracksAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.fisheradelakin.sounddroid.soundcloud.Track;
@@ -44,10 +45,13 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView titleTextView;
+        private ImageView thumbImageView;
+
 
         ViewHolder(View v) {
             super(v);
             titleTextView = (TextView) v.findViewById(R.id.track_title);
+            thumbImageView = (ImageView) v.findViewById(R.id.track_thumbnail);
         }
     }
 }
